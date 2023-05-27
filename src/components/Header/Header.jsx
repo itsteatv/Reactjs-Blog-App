c1364e841dbc062d88825b7410b40e2814b9a46a
itsteatv <rezashow60@gmail.com>
2023-05-27 23:17:17 +0330

    Images Fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import image from "../../assets/image.jpg"
+import image2 from "../../assets/image2.gif"
+import image3 from "../../assets/image3.jpg"
+import image4 from "../../assets/image4.jpg"
+import image5 from "../../assets/image5.jpg"
+import image6 from "../../assets/image6.jpg"
 
 function Header() {
     const settings = {
@@ -31,22 +37,22 @@ function Header() {
         <div style={{ overflow: "hidden" }}>
             <Slider {...settings}>
                 <div>
-                    <img src="https://images.wallpaperscraft.com/image/single/code_text_colorful_140555_1280x720.jpg" alt="image1" style={{ width: "100%" }} />
+                    <img src={image} alt="image1" style={{ width: "100%" }} />
                 </div>
                 <div>
-                    <img src="https://woz-u.com/wp-content/uploads/2022/06/Evolution-of-Coding-scaled.jpg" alt="image2" style={{ width: "100%" }} />
+                    <img src={image2} alt="image2" style={{ width: "100%" }} />
                 </div>
                 <div>
-                    <img src="https://www.codingdojo.com/blog/wp-content/uploads/javacript-1280x720.gif" alt="image3" style={{ width: "100%" }} />
+                    <img src={image3} alt="image3" style={{ width: "100%" }} />
                 </div>
                 <div>
-                    <img src="https://wallpapercropper.com/view/og2R0-119-1280x720.jpg" alt="image4" style={{ width: "100%" }} />
+                    <img src={image4} alt="image4" style={{ width: "100%" }} />
                 </div>
                 <div>
-                    <img src="https://images.wallpaperscraft.com/image/single/laptop_code_programming_212332_1280x720.jpg" alt="image5" style={{ width: "100%" }} />
+                    <img src={image5} alt="image5" style={{ width: "100%" }} />
                 </div>
                 <div>
-                    <img src="https://images.wallpaperscraft.com/image/single/code_programming_text_155013_1280x720.jpg" alt="image6" style={{ width: "100%" }} />
+                    <img src={image6} alt="image6" style={{ width: "100%" }} />
                 </div>
             </Slider>
         </div>
