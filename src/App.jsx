432e4bbe51d1004c72736c6a934666af44f88cc7
itsteatv <rezashow60@gmail.com>
2023-05-27 22:52:21 +0330

    Header Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header"
+
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   )
 
