f69e36d3f95e7c3f7d3d8faffeeb3d12a884770c
itsteatv <rezashow60@gmail.com>
2023-05-28 00:09:25 +0330

    Vercel Images Issue

--- src/components/Header/ImagesPaths.jsx
+++ src/components/Header/ImagesPaths.jsx
@@ -1,11 +1,10 @@
-// ImagePaths.jsx
-const ImagePaths = {
-    image1: "src/assets/image.jpg",
-    image2: "src/assets/image2.gif",
-    image3: "src/assets/image3.jpg",
-    image4: "src/assets/image4.jpg",
-    image5: "src/assets/image5.jpg",
-    image6: "src/assets/image6.jpg",
-};
+import image from "../../assets/image.jpg";
+import image2 from "../../assets/image2.gif";
+import image3 from "../../assets/image3.jpg";
+import image4 from "../../assets/image4.jpg";
+import image5 from "../../assets/image5.jpg";
+import image6 from "../../assets/image6.jpg";
+
+const ImagePaths = [image, image2, image3, image4, image5, image6];
 
 export default ImagePaths;
