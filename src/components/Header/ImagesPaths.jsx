5ac5244b0363a1adf1b575ae4b7523c576deb76d
itsteatv <rezashow60@gmail.com>
2023-06-05 22:08:14 +0330

    Images Path Updated

--- src/components/Header/ImagesPaths.jsx
+++ src/components/Header/ImagesPaths.jsx
@@ -1,9 +1,9 @@
-import image from "../../assets/image.jpg";
-import image2 from "../../assets/image2.gif";
-import image3 from "../../assets/image3.jpg";
-import image4 from "../../assets/image4.jpg";
-import image5 from "../../assets/image5.jpg";
-import image6 from "../../assets/image6.jpg";
+import image from "../../assets/SliderImages/image.jpg";
+import image2 from "../../assets/SliderImages/image2.gif";
+import image3 from "../../assets/SliderImages/image3.jpg";
+import image4 from "../../assets/SliderImages/image4.jpg";
+import image5 from "../../assets/SliderImages/image5.jpg";
+import image6 from "../../assets/SliderImages/image6.jpg";
 
 const ImagePaths = [image, image2, image3, image4, image5, image6];
 
