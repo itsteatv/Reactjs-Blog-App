22cdb10e6fff5b07dd99e4e6aec46220f532bdd5
itsteatv <rezashow60@gmail.com>
2023-06-06 13:55:25 +0330

    Required Data

---
+++ src/assets/data/PostsImagesPaths.jsx
@@ -0,0 +1,13 @@
+import post from "../postsImages/post1.png";
+import post2 from "../postsImages/post2.png";
+import post3 from "../postsImages/post3.png";
+import post4 from "../postsImages/post4.png";
+import post5 from "../postsImages/post5.png";
+import post6 from "../postsImages/post6.png";
+import post7 from "../postsImages/post7.png";
+import post8 from "../postsImages/post8.png";
+import post9 from "../postsImages/post9.png";
+
+const ImagePaths = [post, post2, post3, post4, post5, post6, post7, post8, post9];
+
+export default ImagePaths;
---
+++ src/assets/data/UserImagePath.jsx
@@ -0,0 +1,9 @@
+import user from "../UsersProfile/user.png"
+import user2 from "../UsersProfile/user2.png"
+import user3 from "../UsersProfile/user3.png"
+import user4 from "../UsersProfile/user4.png"
+import user5 from "../UsersProfile/user5.png"
+
+const usersImagePaths = [user, user2, user3, user4, user5];
+
+export default usersImagePaths
