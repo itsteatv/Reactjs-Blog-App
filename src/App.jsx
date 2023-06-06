372f343cad70896ebc4a36a2dc8a184a9c4686cd
itsteatv <rezashow60@gmail.com>
2023-06-06 14:11:19 +0330

    Project Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-// import Posts from "./components/posts/posts";
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
-      {/* <Posts /> */}
     </>
   )
 
