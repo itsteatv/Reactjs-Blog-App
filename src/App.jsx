08eb359a6da6ae043e99a6cd84e1e3680f1cf034
itsteatv <rezashow60@gmail.com>
2023-06-23 01:53:43 +0330

    Single Post Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-import Posts from "./components/AllPosts/Posts"
+import SinglePost from "./components/SinglePost/SinglePost";
+import Posts from "./components/AllPosts/Posts";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Header />
-      <Posts />
-    </>
-  )
-
+      <Switch>
+        <Route exact path="/" component={Header} />
+      </Switch>
+      <Switch>
+        <Route exact path="/" component={Posts} />
+        <Route path="/post/:id" component={SinglePost} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
