663627de2074a58a9de3f63fc324e2ab427d4b5f
itsteatv <rezashow60@gmail.com>
2023-06-23 22:33:13 +0330

    React Router ScrollToTop Issue Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import SinglePost from "./components/SinglePost/SinglePost";
 import Posts from "./components/AllPosts/Posts";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Header} />
-      </Switch>
-      <Switch>
-        <Route exact path="/" component={Posts} />
-        <Route path="/post/:id" component={SinglePost} />
-      </Switch>
+      <ScrollToTop >
+        <Switch>
+          <Route exact path="/" component={Header} />
+        </Switch>
+        <Switch>
+          <Route exact path="/" component={Posts} />
+          <Route path="/post/:id" component={SinglePost} />
+        </Switch>
+      </ScrollToTop>
     </Router>
   );
 }
