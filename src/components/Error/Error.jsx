94d93bcbf368b21b97e742daf494b71d23bf8d49
itsteatv <rezashow60@gmail.com>
2023-06-25 21:19:51 +0330

    Error Component classNames Fixed

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -3,8 +3,8 @@ import styles from "./Error.module.css"
 function ErrorPage() {
     return (
         <section className={styles["error-container"]}>
-            <h1>Error 404: Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+            <h1 className={styles["error-title"]}>Error 404: Page Not Found</h1>
+            <p className={styles["error-desc"]}>The page you are looking for does not exist.</p>
         </section>
     );
 }
