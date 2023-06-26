9d4d0a3fe9750112411e9eea4d9b218fba08185a
itsteatv <rezashow60@gmail.com>
2023-06-26 11:23:19 +0330

    Fixing React Router 404 Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import SinglePost from "./components/SinglePost/SinglePost";
+import Posts from "./components/AllPosts/Posts";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import Error from "./components/Error/Error";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Header />
+              <Posts />
+            </>
+          } />
+          <Route path="/post/:id" element={<SinglePost />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </ScrollToTop>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+////////////////////////////////////////////////////////////////////////////////
+
+/* 
+  !!! This Code Only Works with React Router v5 and it isn't supported by React Router v6 or above
+  but with installing one of react router v5 you can use this code. !!!
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import SinglePost from "./components/SinglePost/SinglePost";
@@ -5,7 +40,6 @@ import Posts from "./components/AllPosts/Posts";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Error from "./components/Error/Error";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 function App() {
   return (
     <Router>
@@ -23,5 +57,5 @@ function App() {
     </Router>
   );
 }
-
 export default App;
+export default App; */
--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,6 +1,26 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+function ScrollToTop({ children }) {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location, navigate]);
+
+    return <>{children}</>;
+}
+
+export default ScrollToTop;
+
+////////////////////////////////////////////////////////////////////////////////
+
+/* 
+  !!! This Code Only Works with React Router v5 and it isn't supported by React Router v6 or above
+  but with installing one of react router v5 you can use this code. !!!
 import React, { useEffect, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-
 function ScrollToTop({ history, children }) {
     useEffect(() => {
         const unlisten = history.listen(() => {
@@ -10,8 +30,7 @@ function ScrollToTop({ history, children }) {
             unlisten();
         }
     }, []);
-
     return <Fragment>{children}</Fragment>;
 }
-
 export default withRouter(ScrollToTop);
+export default withRouter(ScrollToTop); */
