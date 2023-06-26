9ded185f42047431c27bfb692eadf9a469ddf1ad
itsteatv <rezashow60@gmail.com>
2023-06-26 11:04:24 +0330

    React Router 404 Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
-// import Navbar from "./components/Navbar/Navbar"
-// import routes from './components/Routes/Routes';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <ScrollToTop />
-//       <Routes>
-//         {routes.map((route, index) => (
-//           <Route
-//             key={index}
-//             path={route.path}
-//             element={route.element}
-//           />
-//         ))}
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-////////////////////////////////////////////////////////////////////////////////
-
-// !!! This Code Only Works with React Router v5 and it isn't supported by React Router v6 or above
-// but with installing one of react router v5 you can use this code. !!!
-
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import SinglePost from "./components/SinglePost/SinglePost";
--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,25 +1,3 @@
-// import { useEffect } from 'react';
-// import { useLocation, useNavigate } from 'react-router-dom';
-
-// function ScrollToTop({ children }) {
-//     const location = useLocation();
-//     const navigate = useNavigate();
-
-//     useEffect(() => {
-//         window.scrollTo(0, 0);
-//     }, [location, navigate]);
-
-//     return <>{children}</>;
-// }
-
-// export default ScrollToTop;
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-//   !!! This Code Only Works with React Router v5 and it isn't supported by React Router v6 or above
-//   but with installing one of react router v5 you can use this code. !!!
-
 import React, { useEffect, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 
