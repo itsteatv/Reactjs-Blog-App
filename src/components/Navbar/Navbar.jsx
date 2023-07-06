c682a5874f6193b7ac72b1163074a686412ec933
itsteatv <rezashow60@gmail.com>
2023-07-06 15:24:01 +0330

    Register Path Added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
                             <NavLink to="/About" className={({ isActive }) => (isActive ? styles.linkStyle : styles.inActive)}>About</NavLink>
                             <NavLink to="/Contact" className={({ isActive }) => (isActive ? styles.linkStyle : styles.inActive)}>Contact</NavLink>
                             <NavLink to="/Write" className={({ isActive }) => (isActive ? styles.linkStyle : styles.inActive)}>Write</NavLink>
-                            <NavLink to="/login" className={({ isActive }) => (isActive ? styles.linkStyle : styles.inActive)}>Login</NavLink>
+                            <NavLink to="/Register" className={({ isActive }) => (isActive ? styles.linkStyle : styles.inActive)}>Register</NavLink>
                         </ul>
                     </nav>
                     <div className={`${styles.mobileMenuToggle} ${isMobileMenuOpen ? styles.open : ""
