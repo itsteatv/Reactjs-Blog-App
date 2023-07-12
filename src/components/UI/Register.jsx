dce7fee1971a61d8c3a8d021455fa30f43efc981
itsteatv <rezashow60@gmail.com>
2023-07-12 15:31:48 +0330

    Better Regex especially for iranian phone numbers

--- src/components/UI/Register.jsx
+++ src/components/UI/Register.jsx
@@ -48,7 +48,7 @@ function Register() {
         validateAndSetValue: phoneValidateAndSetValue,
         resetValueHandler: resetPhoneHandler,
         formIsValid: phoneIsValid,
-    } = useInput((value) => value.trim().length !== 0);
+    } = useInput((value) => /((0?9)|(\+?989))((14)|(13)|(12)|(19)|(18)|(17)|(15)|(16)|(11)|(10)|(90)|(91)|(92)|(93)|(94)|(95)|(96)|(32)|(30)|(33)|(35)|(36)|(37)|(38)|(39)|(00)|(01)|(02)|(03)|(04)|(05)|(41)|(20)|(21)|(22)|(23)|(31)|(34)|(9910)|(9911)|(9913)|(9914)|(9999)|(999)|(990)|(9810)|(9811)|(9812)|(9813)|(9814)|(9815)|(9816)|(9817)|(998))\W?\d{3}\W?\d{4}/g.test(value.trim()));
 
     const {
         value: enteredPassword,
@@ -111,8 +111,9 @@ function Register() {
                 />
                 <PhoneInput
                     inputProps={{
-                        className: `${styles["register-input"]} ${phoneError ? `${styles.invalid} ${styles.shake}` : ""}`,
+                        className: `${styles["register-input"]} ${styles["selected-flag"]} ${phoneError ? `${styles.invalid} ${styles.shake}` : ""}`,
                         id: "phone",
+
                     }}
                     buttonStyle={{
                         position: "absolute",
@@ -121,7 +122,7 @@ function Register() {
                         bottom: "5px",
                         background: "transparent",
                         border: "none",
-                        borderRadius: "0"
+                        borderRadius: "0",
                     }}
                     dropdownStyle={{
                         outline: "none",
