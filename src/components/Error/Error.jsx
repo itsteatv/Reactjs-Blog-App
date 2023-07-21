07b4d6860f78ed4acfa799cf7abb9780e1fb1076
itsteatv <rezashow60@gmail.com>
2023-07-22 00:16:13 +0330

    404 Page Updated With Help Of mantineui

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,12 +1,75 @@
-import styles from "./Error.module.css"
+import {
+    createStyles,
+    Image,
+    Container,
+    Title,
+    Text,
+    Button,
+    SimpleGrid,
+    rem,
+} from '@mantine/core';
+import image from '../../assets/404Image.svg';
+import { Link } from 'react-router-dom';
 
-function ErrorPage() {
+const useStyles = createStyles((theme) => ({
+    root: {
+        paddingTop: rem(80),
+        paddingBottom: rem(80),
+    },
+
+    title: {
+        fontWeight: 900,
+        fontSize: rem(34),
+        marginBottom: theme.spacing.md,
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+        [theme.fn.smallerThan('sm')]: {
+            fontSize: rem(32),
+        },
+    },
+
+    control: {
+        [theme.fn.smallerThan('sm')]: {
+            width: '100%',
+        },
+    },
+
+    mobileImage: {
+        [theme.fn.largerThan('sm')]: {
+            display: 'none',
+        },
+    },
+
+    desktopImage: {
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
+    },
+}));
+
+export function Error() {
+    const { classes } = useStyles();
+
     return (
-        <section className={styles["error-container"]}>
-            <h1 className={styles["error-title"]}>Error 404: Page Not Found</h1>
-            <p className={styles["error-desc"]}>The page you are looking for does not exist.</p>
-        </section>
+        <Container className={classes.root}>
+            <SimpleGrid spacing={80} cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1, spacing: 40 }]}>
+                <Image src={image} className={classes.mobileImage} />
+                <div>
+                    <Title className={classes.title}>Something is not right...</Title>
+                    <Text color="dimmed" size="lg">
+                        Page you are trying to open does not exist. You may have mistyped the address, or the
+                        page has been moved to another URL. If you think this is an error contact support.
+                    </Text>
+                    <Link to="/">
+                        <Button variant="outline" size="md" mt="xl" className={classes.control}>
+                            Get back to home page
+                        </Button>
+                    </Link>
+                </div>
+                <Image src={image} className={classes.desktopImage} />
+            </SimpleGrid>
+        </Container>
     );
 }
 
-export default ErrorPage;
+export default Error;
