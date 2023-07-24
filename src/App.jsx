b0712a62f9eba3cd4c027caec506f007fa444be0
itsteatv <rezashow60@gmail.com>
2023-07-24 16:51:57 +0330

    Routing Changed Based On Registered Or Logged In User

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import SinglePost from "./components/SinglePost/SinglePost";
@@ -7,20 +7,12 @@ import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Error from "./components/Error/Error";
 import Login from "./components/UI/Login";
 import Register from './components/UI/Register';
-import Dashboard from "./components/Dashboard/Dashboard"
+import Dashboard from "./components/Dashboard/Dashboard";
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
 
 function App() {
   const isRegistered = useSelector((state) => state.register.isRegistered);
   const isLoggedIn = useSelector((state) => state.register.isLoggedIn);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isRegistered || isLoggedIn) {
-      navigate('/dashboard');
-    }
-  }, [isRegistered, navigate, isLoggedIn]);
 
   return (
     <>
@@ -34,8 +26,18 @@ function App() {
             </>
           } />
           <Route path="/post/:id" element={<SinglePost />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/register"
+            element={
+              isRegistered || isLoggedIn ? <Dashboard /> : <Register />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              isRegistered || isLoggedIn ? <Dashboard /> : <Login />
+            }
+          />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<Error />} />
         </Routes>
