2d6dfb281a3542eea121d7137f43a2d76451474b
itsteatv <rezashow60@gmail.com>
2023-07-26 12:00:30 +0330

    Now Editing User Data Is Possible

--- src/components/Dashboard/UpdateProfile.jsx
+++ src/components/Dashboard/UpdateProfile.jsx
@@ -1,15 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchUserData } from '../store/userSlice';
 import { toast } from 'react-toastify';
 import ResultPage from "../UI/ResultPage"
 import styles from './UpdateProfile.module.css';
 import Skeleton from '@mui/material/Skeleton';
+import Cookies from 'js-cookie';
 
 function UpdateProfile() {
     const dispatch = useDispatch();
     const { data: userData, loading, error } = useSelector((state) => state.user);
 
+    const [formData, setFormData] = useState({
+        name: '',
+        username: '',
+        email: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
 
     useEffect(() => {
         try {
@@ -19,8 +33,41 @@ function UpdateProfile() {
         }
     }, [dispatch]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    useEffect(() => {
+        if (userData) {
+            setFormData({
+                name: userData.name,
+                username: userData.username,
+                email: userData.email,
+            });
+        }
+    }, [userData]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const authHeader = `Bearer ${Cookies.get("token")}`;
+
+        try {
+            const response = await fetch("https://neisiali.ir/api/user", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: authHeader,
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+            if (!response.ok) {
+                throw new Error("Couldn't Send Data to API");
+            }
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -43,26 +90,32 @@ function UpdateProfile() {
                             type="text"
                             id="name"
                             name="name"
-                            defaultValue={userData ? userData.name : ""}
+                            // defaultValue={userData ? userData.name : ""}
+                            value={formData.name}
+                            onChange={handleChange}
                             className={styles['form-input']}
                             placeholder="Your Name"
                         />
 
-                        <input
+                        {/* <input
                             type="text"
                             id="username"
                             name="username"
-                            defaultValue={userData ? userData.username : ""}
+                            // defaultValue={userData ? userData.username : ""}
                             className={styles['form-input']}
+                            value={formData.username}
+                            onChange={handleChange}
                             placeholder="Your Username"
-                        />
+                        /> */}
 
                         <input
                             type="email"
                             id="email"
                             name="email"
-                            defaultValue={userData ? userData.email : ""}
+                            // defaultValue={userData ? userData.email : ""}
                             className={styles['form-input']}
+                            value={formData.email}
+                            onChange={handleChange}
                             placeholder="Your Email"
                         />
 
