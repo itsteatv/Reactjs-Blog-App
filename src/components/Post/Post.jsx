597571e1652a3ae08ae6c68031cae8a8b961a5d3
itsteatv <rezashow60@gmail.com>
2023-08-05 19:27:40 +0330

    Loading Spinner Added Before Loading Blog Posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import { Skeleton } from "@mui/material";
 import { toast } from "react-toastify";
 import { format } from "date-fns";
+import { Skeleton } from "@mui/material";
+import { Waveform } from '@uiball/loaders'
 import Cookies from "js-cookie";
 import styles from "./Post.module.css";
 import Pagination from "../UI/Pagination";
@@ -57,90 +58,102 @@ function SinglePost() {
     const currentPosts = allPosts.slice(indexOfFirstPost, indexOfLastPost);
     console.log(indexOfLastPost, indexOfFirstPost, currentPosts);
 
-    // Change page
     const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return (
         <>
-            <div className={styles["single-post"]}>
-                {currentPosts.map((post) => (
-                    <Link to={`/post/${post.id}`} key={post.id} className={styles.link}>
-                        <div className={styles.container}>
-                            {isLoading ? (
-                                <Skeleton
-                                    className={styles.image}
-                                    variant="rounded"
-                                    width="100%"
-                                />
-                            ) : (
-                                <img className={styles.image} src={post.image} alt="" />
-                            )}
-                            <div className={styles.desc}>
-                                <div className={styles["badge-container"]}>
+            {!isLoading ?
+                <div className={styles.loading}>
+                    <Waveform
+                        size={40}
+                        lineWeight={3.5}
+                        speed={1}
+                        color="black"
+                    />
+                </div>
+                :
+                <>
+                    <div className={styles["single-post"]}>
+                        {currentPosts.map((post) => (
+                            <Link to={`/post/${post.id}`} key={post.id} className={styles.link}>
+                                <div className={styles.container}>
                                     {isLoading ? (
                                         <Skeleton
-                                            className={styles.badge}
-                                            variant="text"
+                                            className={styles.image}
+                                            variant="rounded"
+                                            width="100%"
                                         />
                                     ) : (
-                                        post.categories.map((category) => (
-                                            <span key={category.slug} className={styles.badge}>
-                                                {category.name}
-                                            </span>
-                                        ))
+                                        <img className={styles.image} src={post.image} alt="" />
                                     )}
+                                    <div className={styles.desc}>
+                                        <div className={styles["badge-container"]}>
+                                            {isLoading ? (
+                                                <Skeleton
+                                                    className={styles.badge}
+                                                    variant="text"
+                                                />
+                                            ) : (
+                                                post.categories.map((category) => (
+                                                    <span key={category.slug} className={styles.badge}>
+                                                        {category.name}
+                                                    </span>
+                                                ))
+                                            )}
+                                        </div>
+                                        {isLoading ? (
+                                            <Skeleton
+                                                className={styles.title}
+                                                variant="text"
+                                            />
+                                        ) : (
+                                            <p className={styles.title}>{post.name}</p>
+                                        )}
+                                    </div>
+                                    <div className={styles.user}>
+                                        {isLoading ? (
+                                            <Skeleton
+                                                className={styles.userImage}
+                                                variant="circular"
+                                                animation="wave"
+                                                width={70}
+                                                height={40}
+                                            />
+                                        ) : (
+                                            <img className={styles["author-image"]} src={post.user.image} alt="user profile" />
+                                        )}
+                                        {isLoading ? (
+                                            <Skeleton
+                                                className={styles.username}
+                                                variant="text"
+                                                width="80%"
+                                            />
+                                        ) : (
+                                            <p className={styles.username}>{post.user.name}</p>
+                                        )}
+                                        {isLoading ? (
+                                            <Skeleton
+                                                className={styles.date}
+                                                variant="text"
+                                                width="50%"
+                                            />
+                                        ) : (
+                                            <p className={styles.date}>
+                                                {format(new Date(post.created_at), "MMMM d, yyyy")}
+                                            </p>
+                                        )}
+                                    </div>
                                 </div>
-                                {isLoading ? (
-                                    <Skeleton
-                                        className={styles.title}
-                                        variant="text"
-                                    />
-                                ) : (
-                                    <p className={styles.title}>{post.name}</p>
-                                )}
-                            </div>
-                            <div className={styles.user}>
-                                {isLoading ? (
-                                    <Skeleton
-                                        className={styles.userImage}
-                                        variant="circular"
-                                        animation="wave"
-                                        width={70}
-                                        height={40}
-                                    />
-                                ) : (
-                                    <img className={styles["author-image"]} src={post.user.image} alt="user profile" />
-                                )}
-                                {isLoading ? (
-                                    <Skeleton
-                                        className={styles.username}
-                                        variant="text"
-                                        width="80%"
-                                    />
-                                ) : (
-                                    <p className={styles.username}>{post.user.name}</p>
-                                )}
-                                {isLoading ? (
-                                    <Skeleton
-                                        className={styles.date}
-                                        variant="text"
-                                        width="50%"
-                                    />
-                                ) : (
-                                    <p className={styles.date}>
-                                        {format(new Date(post.created_at), "MMMM d, yyyy")}
-                                    </p>
-                                )}
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-            </div>
-            <Pagination
-                postsPerPage={postPerPage}
-                totalPosts={allPosts.length}
-                paginate={paginate}
-            />
+                            </Link>
+                        ))}
+                    </div>
+                    <Pagination
+                        postsPerPage={postPerPage}
+                        totalPosts={allPosts.length}
+                        paginate={paginate}
+                    />
+                </>
+            }
         </>
     );
 }
